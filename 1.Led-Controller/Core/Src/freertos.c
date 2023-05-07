78809941905f14fb34d33f2e674eb728ed783623
Subhadeep <subhadeepdhang1@gmail.com>
2023-05-07 19:26:20 +0530

    osTaskSuspend(taskId) not working when calling from ISR , need to implement binary-semaphore or task notification

--- 1.Led-Controller/Core/Src/freertos.c
+++ 1.Led-Controller/Core/Src/freertos.c
@@ -29,9 +29,9 @@
 osThreadId LedOnTaskHandle;
 osThreadId LedOffTaskHandle;
 osThreadId LedBlinkTaskHandle;
+osMessageQId MyQueueHandle;
 
-char rxMessege[100];
-char singleByte;
+char rxMessage[6];
 
 /* Private function prototypes -----------------------------------------------*/
 void ledOnTask_init(void const *argument);
@@ -62,25 +62,31 @@ void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer, StackTyp
  * @retval None
  */
 void MX_FREERTOS_Init(void)
-{
+{	
+	/*Create simple Queue to pass single 16bit integer*/
+	osMessageQDef(myQueue, 1, uint16_t);
+	MyQueueHandle = osMessageCreate(osMessageQ(myQueue), NULL);
+
 	/* Create the thread(s) */
 	/* definition and creation of LedOnTask */
-	osThreadDef(LedOnTask, ledOnTask_init, osPriorityNormal, 0, 128);
+	osThreadDef(LedOnTask, ledOnTask_init, osPriorityAboveNormal, 0, 128);
 	LedOnTaskHandle = osThreadCreate(osThread(LedOnTask), NULL);
 
 	/* definition and creation of LedOffTask */
-	osThreadDef(LedOffTask, ledOffTask_init, osPriorityNormal, 0, 128);
+	osThreadDef(LedOffTask, ledOffTask_init, osPriorityHigh, 0, 128);
 	LedOffTaskHandle = osThreadCreate(osThread(LedOffTask), NULL);
 
 	/* definition and creation of LedBlinkTask */
 	osThreadDef(LedBlinkTask, ledBlinkTask_init, osPriorityNormal, 0, 128);
 	LedBlinkTaskHandle = osThreadCreate(osThread(LedBlinkTask), NULL);
 
-	/*configure interrupt to recieve single bytes via uart*/
-	HAL_UART_Receive_IT(&huart1, (uint8_t *)&singleByte, 1);
+	/*configure interrupt to recieve six bytes via uart*/
+	HAL_UART_Receive_IT(&huart1, (uint8_t *)&rxMessage, 6);
 
-
-	/*Suspend all task*/
+	/*Suspend all task before the start of the kernel*/
+	osThreadSuspend(LedOnTaskHandle);
+	osThreadSuspend(LedOffTaskHandle);
+	osThreadSuspend(LedBlinkTaskHandle);
 }
 
 
@@ -92,14 +98,11 @@ void MX_FREERTOS_Init(void)
  */
 /* USER CODE END Header_ledOnTask_init */
 void ledOnTask_init(void const *argument)
-{
-	/* USER CODE BEGIN ledOnTask_init */
-	/* Infinite loop */
-	for (;;)
-	{
-		osDelay(1);
+{	
+	for (;;) {	
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET);
+		osDelay(1000);
 	}
-	/* USER CODE END ledOnTask_init */
 }
 
 
@@ -109,15 +112,13 @@ void ledOnTask_init(void const *argument)
  * @retval None
  */
 void ledOffTask_init(void const *argument)
-{
-	/* USER CODE BEGIN ledOffTask_init */
-	/* Infinite loop */
-	for (;;)
-	{
-		osDelay(1);
+{	
+	for (;;) {	
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_SET);
+		osDelay(1000);
 	}
-	/* USER CODE END ledOffTask_init */
 }
+
 
 /**
  * @brief Function implementing the LedBlinkTask thread.
@@ -125,14 +126,20 @@ void ledOffTask_init(void const *argument)
  * @retval None
  */
 void ledBlinkTask_init(void const *argument)
-{
-	/* USER CODE BEGIN ledBlinkTask_init */
-	/* Infinite loop */
-	for (;;)
-	{
-		osDelay(1);
+{	
+	osEvent myEvent;
+	volatile uint16_t duration;
+
+	for (;;) {
+		for (volatile uint8_t i = 0; i < 1; i++) {	
+			myEvent = osMessageGet(MyQueueHandle,10);
+			duration = myEvent.value.v;
+		}
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET);
+		osDelay(duration);
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_SET);
+		osDelay(duration);
 	}
-	/* USER CODE END ledBlinkTask_init */
 }
 
 
@@ -142,16 +149,46 @@ void ledBlinkTask_init(void const *argument)
  * @retval None
  */
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
-{
-	volatile static uint8_t index = 0;
-	rxMessege[index] = singleByte; 
-	index++;
+{	
+	volatile uint16_t duration;
+	volatile static uint8_t runningTaskId = 0;
 
-	if (strncmp(rxMessege, "LED_ON", 6) == 0) {
+	/*configure interrupt to recieve six bytes via uart*/
+	HAL_UART_Receive_IT(&huart1, (uint8_t *)&rxMessage, 6);
 
-	} else if (strncmp(rxMessege, "LED_OFF", 6) == 0) {
+	/*suspend all 3 task and setup interrupt for receiving another byte through byte*/
+	switch (runningTaskId) {
+	case 1:
+		if (osThreadSuspend(LedOnTaskHandle) != osOK) {
+			HAL_UART_Transmit(&huart1, (uint8_t *)"ERROR\n", 6, 100);
+		}
+		break;
+	case 2:
+		osThreadSuspend(LedOffTaskHandle);
+		break;
+	case 3:
+		osThreadSuspend(LedBlinkTaskHandle);
+		break;
+	}
 
-	} else if (strncmp(rxMessege, "LED_BLINK", 6) == 0) {
-
+	/*run 1 task and suspend other 2 according to the received data*/
+	if (strncmp(rxMessage, "110000", 6) == 0) {
+		osThreadResume(LedOnTaskHandle);
+		runningTaskId = 1;
+		
+	} else if (strncmp(rxMessage, "000000", 6) == 0) {
+		osThreadResume(LedOffTaskHandle);
+		runningTaskId = 2;
+		
+	} else if (strncmp(rxMessage, "22", 2) == 0) {		
+		/*convert blink duration from string to int*/
+		duration  = (rxMessage[2] - 48) * 1000;
+		duration += (rxMessage[3] - 48) * 100;
+		duration += (rxMessage[4] - 48) * 10;
+		duration += (rxMessage[5] - 48) * 1;
+		/*pass the duration value to LedBlinkTask through message queue*/
+		osMessagePut(MyQueueHandle, duration, osWaitForever);
+		osThreadResume(LedBlinkTaskHandle);
+		runningTaskId = 3;
 	}
 }
